ddea0e904a92a60ba38ed14a823f1d5fe828e0ef
Kazuki Ono <kazuki.1209.haru@gmail.com>
2019-11-27 17:22:23 +0900

    setter/getter -> Lombok @Data　に変更

--- src/main/java/com/tuyano/springboot/HelloController.java
+++ src/main/java/com/tuyano/springboot/HelloController.java
@@ -3,6 +3,7 @@ package com.tuyano.springboot;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import lombok.Data;
 
 @RestController
 public class HelloController {
@@ -19,7 +20,7 @@ public class HelloController {
 		return new DataObject(id, names[id],mails[id]);
 	}
 }
-
+@Data
 class DataObject {
 	private int id;
 	private String name;
@@ -30,30 +31,5 @@ class DataObject {
 		this.id = id;
 		this.name = name;
 		this.value = value;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getValue() {
-		return value;
-	}
-
-	public void setValue(String value) {
-		this.value = value;
-	}
-	
+	}	
 }
